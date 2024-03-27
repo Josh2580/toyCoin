08bf11e24f8f850ddded6dc58ef1b5c9892ef273
Joshua <joshuaugwuede@gmail.com>
2024-03-27 13:56:47 +0100

    onBoard struncture

--- src/pages/OnboardingPage3.jsx
+++ src/pages/OnboardingPage3.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import SampleImg from "../assets/sample.png";
+import { useNavigate } from "react-router-dom";
 
 const OnboardingPage3 = () => {
-  return <div>OnboardingPage3</div>;
+  const navigate = useNavigate();
+
+  const ClickHandler = () => {
+    navigate("/home");
+  };
+
+  return (
+    <div className="flex flex-col bg-violet-600 h-screen justify-between p-10">
+      <h2 className="text-white text-4xl text-center">
+        Toy the Aggregator Dex on Ton
+      </h2>
+      <img src={SampleImg} alt="Onboard Toy Image" />
+      <button
+        className="bg-white p-2.5 text-xl rounded-md"
+        onClick={ClickHandler}
+      >
+        Earn Toy
+      </button>
+    </div>
+  );
 };
 
 export default OnboardingPage3;
