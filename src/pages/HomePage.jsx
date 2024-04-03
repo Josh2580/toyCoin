1539e61a361d07e647c3943c42c5b3a8a1f27c41
Joshua <joshuaugwuede@gmail.com>
2024-04-03 12:35:49 +0100

    home bg

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,7 +6,7 @@ import More from "../components/More";
 
 const HomePage = () => {
   return (
-    <div className=" h-screen flex flex-col gap-4 justify-between">
+    <div className=" h-screen flex flex-col gap-4 justify-between bg-gray-100">
       <div className="m-3 mx-6 flex flex-col justify-between h-full ">
         <HeaderComp />
         <Profile />
