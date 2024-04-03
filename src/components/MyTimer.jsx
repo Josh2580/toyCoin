51b1492b43686b8041d4418442792bfc3f7887f1
Joshua <joshuaugwuede@gmail.com>
2024-04-03 20:49:39 +0100

    claim page

---
+++ src/components/MyTimer.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { useTimer } from "react-timer-hook";
+
+const MyTimer = ({ expiryTimestamp }) => {
+  const {
+    totalSeconds,
+    seconds,
+    minutes,
+    hours,
+    days,
+    isRunning,
+    start,
+    pause,
+    resume,
+    restart,
+  } = useTimer({
+    expiryTimestamp,
+    onExpire: () => console.warn("onExpire called"),
+  });
+
+  return (
+    <div style={{ textAlign: "center" }}>
+      <p>{isRunning ? "Running" : "Not running"}</p>
+
+      <div style={{ fontSize: "30px" }}>
+        <span>{days}</span>:<span>{hours}</span>:<span>{minutes}</span>:
+        <span>{seconds}</span>
+      </div>
+      <div className="flex gap-2 text-sm">
+        {/* <button onClick={start}>Start</button>
+        <button onClick={pause}>Pause</button>
+        <button onClick={resume}>Resume</button> */}
+        <button
+          className="text-center  w-full"
+          onClick={() => {
+            // Restarts to 5 minutes timer
+            const time = new Date();
+            time.setSeconds(time.getSeconds() + 300);
+            restart(time);
+          }}
+        >
+          Restart
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default MyTimer;
--- src/pages/ClaimPage.jsx
+++ src/pages/ClaimPage.jsx
@@ -4,8 +4,12 @@ import Footer2 from "./../components/Footer2";
 
 import CheckImg from "../assets/home3.png";
 import SampleImg from "../assets/sample.png";
+import MyTimer from "./../components/MyTimer";
 
 const ClaimPage = () => {
+  const time = new Date();
+  time.setSeconds(time.getSeconds() + 600); // 10 minutes timer
+
   return (
     <div className=" h-screen flex flex-col gap-4 p-6 justify-between bg-gray-100">
       <HeaderComp />
@@ -17,9 +21,8 @@ const ClaimPage = () => {
         <div className=" w-max">
           <img src={CheckImg} className="icon-img " alt="Check Image" />
         </div>
-        <div className="flex flex-1 flex-col  w-max ">
-          <p>Storage</p>
-          <p>Date</p>
+        <div className="flex flex-1 flex-col  w-10">
+          <MyTimer expiryTimestamp={time} />
         </div>
         <button className="primary-btn">Claim Toy</button>
       </div>
