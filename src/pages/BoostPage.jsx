b7857c2f0a7117a9b062a32fe479f3db6dc9ea63
Joshua <joshuaugwuede@gmail.com>
2024-04-17 19:26:42 +0100

    boost page

--- src/pages/BoostPage.jsx
+++ src/pages/BoostPage.jsx
@@ -6,7 +6,7 @@ const BoostPage = () => {
   return (
     <div className=" h-screen flex flex-col gap-4 p-6 justify-between bg-gray-100">
       <HeaderComp visible={true} />
-      <h2 className="text-center text-5xl text-bold">Boost Coming Coming</h2>
+      <h2 className="text-center text-5xl text-bold">Boost Coming Soon</h2>
       <Footer2 />
     </div>
   );
